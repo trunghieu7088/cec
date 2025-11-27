5c2594a97ac172d44292e5d8a33c9e37cd252c3a
trunghieu7088 <lamtrunghieu366@gmail.com>
2025-11-27 16:22:16 +0700

    upload authorize SDK

---
+++ continuinged-child/inc/llms/authorize/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'authorizenet/authorizenet' => array(
+            'pretty_version' => '2.0.4',
+            'version' => '2.0.4.0',
+            'reference' => '8555cc245953dd0ac57f7ea424a5572eae4c7191',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../authorizenet/authorizenet',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
